0b5ef583b8b43b06f4f74afd29cdee83ee05ea62
Henoc Musau <henocmusau@gmail.com>
2024-02-24 18:06:02 +0100

    Mise en place de la responsivité de la navbar

--- componets/Header/Header.tsx
+++ componets/Header/Header.tsx
@@ -1,21 +1,25 @@
-import React from 'react'
+'use client'
+
+import React, { useState } from 'react'
 import { Navbar } from '..'
 import Image from 'next/image'
 import logo from '@/assets/logos/logoWhiteAll.svg'
 import { AlignCenter, Search } from 'lucide-react'
 
 export default function Header() {
+    const [navOpen, setNavOpen] = useState(false)
+    const closeNav = () => setNavOpen(false)
     return (
         <header className='header'>
             <div className='container header__container'>
-                <button className='hamburger'>
+                <button onClick={() => setNavOpen(true)} className='hamburger'>
                     <AlignCenter />
                 </button>
                 <strong className='brand'>
                     <Image className='brand__image' src={logo} alt='Helka TP logo' />
                 </strong>
 
-                <Navbar />
+                <Navbar isOpen={navOpen} closeNav={closeNav} />
 
                 <section className='header__aside'>
                     <button>
--- componets/Header/Navbar.tsx
+++ componets/Header/Navbar.tsx
@@ -1,17 +1,27 @@
+'use client'
 import { navbarLinks } from '@/constants/navlinks'
 import React from 'react'
 import { Navlink } from '..'
+import { X } from 'lucide-react'
 
-navbarLinks
+interface Props {
+    isOpen: boolean
+    closeNav: () => void
+}
 
-export default function Navbar() {
+export default function Navbar({ isOpen, closeNav }: Props) {
     return (
-        <nav className='navbar'>
-            {
-                navbarLinks.map((navlink) => (
-                    <Navlink key={navlink.link} text={navlink?.text} link={navlink.link} />
-                ))
-            }
-        </nav>
+        <>
+            <button className='closeBtn' onClick={closeNav}>
+                <X />
+            </button>
+            <nav className={isOpen ? 'navbar navbarActive' : 'navbar'}>
+                {
+                    navbarLinks.map((navlink) => (
+                        <Navlink key={navlink.link} text={navlink?.text} link={navlink.link} />
+                    ))
+                }
+            </nav>
+        </>
     )
 }
